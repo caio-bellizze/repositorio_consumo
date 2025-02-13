7049ea089002b71bdb170a833589382dff27f31f
caio-bellizze <ocaiobellizze@gmail.com>
2025-02-13 18:51:45 -0300

    consumo_v14.py

--- consumo_v14.py
+++ consumo_v14.py
@@ -76,17 +76,17 @@ if st.button("Calcular") and empresa_filtro:
     ax.legend(title=f"Flexibilidade Estimada: {flexibilidade_estimativa:.2f}%", loc="center right")
     
     # Adicionando linha divisória entre anos
-    ax.axvline(x=11.5, color='green', linestyle='dashed', ymin=0, ymax=2)  # Linha divisória entre os anos
+    ax.axvline(x=11.5, color='black', linestyle='dashed', ymin=0, ymax=1)  # Linha divisória entre os anos
+    ax.axvline(x=11.5, color='black', linestyle='dashed', ymin=1, ymax=2)  # Linha divisória entre os anos
 
     # Ajustando os rótulos do eixo X
     ax.set_xticklabels(df_mensal["Ano_Mes"], rotation=90)
-    ax.set_xlabel("Data")
     ax.set_ylabel("Consumo Médio Total")
     ax.set_title(f"Consumo Histórico - {empresa_filtro}")
     
     # Adicionando os anos abaixo do eixo X
     for i, ano in enumerate(["2022" , "2023", "2024"]):
-        ax.text(i * 12 + 5.5, -4, ano, fontsize=12, ha="center", color="black")  # Y reduzido para afastar mais
+        ax.text(i * 12 + 5.5, -1, ano, fontsize=12, ha="center", color="black")  # Y reduzido para afastar mais
 
     ax.grid(True, linestyle="--", alpha=0.5)
 
