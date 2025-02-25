2aee9ee0e647a2290b887e2b284adb5d443a167d
caio-bellizze <ocaiobellizze@gmail.com>
2025-02-25 16:52:36 -0300

    consumo_v15.py

--- consumo_v15.py
+++ consumo_v15.py
@@ -129,7 +129,6 @@ if st.button("Calcular") and empresa_filtro:
             "Cidade": [tabela_df["CIDADE"].mode()[0]],  # Cidade mais frequente
             "Estado": [tabela_df["ESTADO_UF"].mode()[0]],  # Estado mais frequente
             "Ramo": [", ".join(tabela_df["RAMO_ATIVIDADE"].unique())],  # Concatenar ramos únicos
-            "Consumo Médio Total": [tabela_df["Consumo Médio Total"].sum()]
         })
 
         return tabela_final
@@ -144,17 +143,4 @@ if st.button("Calcular") and empresa_filtro:
     st.write("### 📋 Informações da Empresa")
     st.dataframe(tabela, hide_index=True)
 
-    # 🔹 Exibir mais informações de todas as unidades
-    if st.button("Exibir mais"):
-        tabela_unidades = df_empresa.groupby("CNPJ_CARGA").agg({
-            "SIGLA_PARCELA_CARGA": "count",
-            "CIDADE": "first",
-            "ESTADO_UF": "first",
-            "RAMO_ATIVIDADE": lambda x: ", ".join(x.unique()),
-            "Consumo Médio Total": "sum"
-        }).reset_index()
-
-        tabela_unidades["CNPJ_CARGA"] = tabela_unidades["CNPJ_CARGA"].apply(lambda x: format_cnpj(str(x)))
-
-        st.write("### 📋 Informações de Todas as Unidades")
-        st.dataframe(tabela_unidades, hide_index=True)
+   
