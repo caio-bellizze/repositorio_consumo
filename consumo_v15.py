b6be2530cfbae3d8bd69c2edada4fc006ac39322
caio-bellizze <ocaiobellizze@gmail.com>
2025-02-25 16:35:03 -0300

    consumo_v15.py

--- consumo_v15.py
+++ consumo_v15.py
@@ -7,7 +7,7 @@ from scipy.stats.mstats import winsorize
 import openpyxl
 
 # 🔹 Função para formatar o CNPJ corretamente
-def format_cnpj(cnpj):
+def format_cnpj(CNPJ):
     cnpj = str(int(cnpj))  # Garantir que o CNPJ seja tratado como inteiro para evitar ".0"
     cnpj = f"{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}"
     return cnpj
