c2389bd8c20e8f8666a951dbe4384a2645f55655
caio-bellizze <ocaiobellizze@gmail.com>
2025-02-26 16:31:06 -0300

    consumo_v15.py

--- consumo_v15.py
+++ consumo_v15.py
@@ -128,18 +128,18 @@ if st.button("Calcular") and empresa_filtro:
         # Pegando o CNPJ da Matriz (o menor CNPJ geralmente é o da matriz)
         cnpj_matriz = tabela_df["CNPJ_CARGA"].astype(str).min().split(".")[0]  # Remover casas decimais indesejadas
 
-         # Filtrar a tabela para obter a cidade e o estado correspondentes ao CNPJ da matriz
-    cidade_matriz = tabela_df[tabela_df["CNPJ_CARGA"].astype(str).str.startswith(cnpj_matriz)]["CIDADE"].values[0]
-    estado_matriz = tabela_df[tabela_df["CNPJ_CARGA"].astype(str).str.startswith(cnpj_matriz)]["ESTADO_UF"].values[0]
+        # Filtrar a tabela para obter a cidade e o estado correspondentes ao CNPJ da matriz
+        cidade_matriz = tabela_df[tabela_df["CNPJ_CARGA"].astype(str).str.startswith(cnpj_matriz)]["CIDADE"].values[0]
+        estado_matriz = tabela_df[tabela_df["CNPJ_CARGA"].astype(str).str.startswith(cnpj_matriz)]["ESTADO_UF"].values[0]
 
-    # Criar nova tabela consolidada
-    tabela_final = pd.DataFrame({
-        "CNPJ": [format_cnpj(cnpj_matriz)],
-        "Unidades": [tabela_df["SIGLA_PARCELA_CARGA"].sum()],
-        "Cidade": [cidade_matriz],  # Cidade correspondente ao CNPJ da matriz
-        "Estado": [estado_matriz],  # Estado correspondente ao CNPJ da matriz
-        "Ramo": [", ".join(tabela_df["RAMO_ATIVIDADE"].unique())],  # Concatenar ramos únicos
-    })
+        # Criar nova tabela consolidada
+        tabela_final = pd.DataFrame({
+            "CNPJ": [format_cnpj(cnpj_matriz)],
+            "Unidades": [tabela_df["SIGLA_PARCELA_CARGA"].sum()],
+            "Cidade": [cidade_matriz],  # Cidade correspondente ao CNPJ da matriz
+            "Estado": [estado_matriz],  # Estado correspondente ao CNPJ da matriz
+            "Ramo": [", ".join(tabela_df["RAMO_ATIVIDADE"].unique())],
+             })
 
         return tabela_final
 
@@ -152,3 +152,4 @@ if st.button("Calcular") and empresa_filtro:
     tabela = buscar_informacoes(df_empresa)
     st.write("### 📋 Informações da Empresa")
     st.dataframe(tabela, hide_index=True)
+        
