38dd4032ca707f5aa9e9d79c7ee9b66d5c533eae
caio-bellizze <ocaiobellizze@gmail.com>
2025-03-06 17:21:18 -0300

    consumo_v14.py

--- consumo_v14.py
+++ consumo_v14.py
@@ -175,7 +175,7 @@ if not df_ultimos_12_meses.empty and "Consumo Médio Total" in df_ultimos_12_mes
     tabela_consumo_submercado = tabela_consumo_submercado.drop(columns=["Consumo (MWm 12 meses)"])
     
     # 🔹 Exibir tabela no Streamlit
-    st.write("### 🇧🇷 Percentual de Consumo por Submercado")
+    st.write("### 🌎 Percentual de Consumo por Submercado")
     st.dataframe(tabela_consumo_submercado, hide_index=True)
 else:
     st.warning("Nenhum dado disponível para calcular o consumo por submercado.")
