e8b5d451ff0c80c47f436849e3420679e3ed1962
caio-bellizze <ocaiobellizze@gmail.com>
2025-04-01 17:12:35 -0300

    consumo_v2.01.py

--- consumo_v2.01.py
+++ consumo_v2.01.py
@@ -7,7 +7,7 @@ limit = 1000000000
 cnpj = ""
 cnpj_limpo = cnpj.replace(".", "").replace("/", "").replace("-", "")
 query = cnpj_limpo
-url = f"https://dadosabertos.ccee.org.br/api/3/action/datastore_search?resource_id=b854f7bc-94a3-423a-96b7-2d4756ec77d1&limit={limit}&q={query}"
+url = f"https://dadosabertos.ccee.org.br/api/3/action/datastore_search?resource_id=b854f7bc-94a3-423a-96b7-2d4756ec77d1&limit={limit}"
  
 response = requests.get(url)
 data = response.json()
