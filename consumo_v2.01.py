4481ad22464f9a45909d48ae4143b2a62877e0a9
caio-bellizze <ocaiobellizze@gmail.com>
2025-04-02 15:09:48 -0300

    consumo_v2.01.py

--- consumo_v2.01.py
+++ consumo_v2.01.py
@@ -5,7 +5,7 @@ import streamlit as st
 # URL base da API
 base_url = "https://dadosabertos.ccee.org.br/api/3/action/datastore_search"
 resource_id = "b854f7bc-94a3-423a-96b7-2d4756ec77d1"  # ID do conjunto de dados
-limit = 1000  # Número de registros por requisição (ajustável)
+limit = 10000  # Número de registros por requisição (ajustável)
 offset = 0  # Inicia do primeiro registro
 all_records = []  # Lista para armazenar todos os registros
 
